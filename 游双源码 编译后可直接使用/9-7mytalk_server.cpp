90bd467282d22aae495e2f3cb2c8f85d82d0872e
Mr-whoz <1124333578@qq.com>
2018-06-18 15:49:22 +0800

    源码上传
    因为之前想把这个改为epoll实现的，所以改了源码。第一次上传时没有注意，把自己的epoll版本（有bug）的当做游双源码上传了，这里重新上传一下游双源码

--- 游双源码 编译后可直接使用/9-7mytalk_server.cpp
+++ 游双源码 编译后可直接使用/9-7mytalk_server.cpp
@@ -10,7 +10,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <sys/epoll.h>
+#include <poll.h>
 
 #define USER_LIMIT 5
 #define BUFFER_SIZE 64
@@ -31,18 +31,6 @@ int setnonblocking( int fd )
     return old_option;
 }
 
-void addfd( int epollfd, int fd, bool enable_et )
-{
-	epoll_event event;
-	event.data.fd = fd;
-	event.events = EPOLLIN|EPOLLRDHUP|EPOLLERR;
-	if( enable_et )
-	{
-		event.events |= EPOLLET;
-	}
-	epoll_ctl( epollfd, EPOLL_CTL_ADD, fd, &event );
-	setnonblocking( fd );
-}
 int main( int argc, char* argv[] )
 {
     if( argc <= 2 )
@@ -70,28 +58,29 @@ int main( int argc, char* argv[] )
     assert( ret != -1 );
 
     client_data* users = new client_data[FD_LIMIT];
-    struct epoll_event events[USER_LIMIT+1];
+    pollfd fds[USER_LIMIT+1];
     int user_counter = 0;
     for( int i = 1; i <= USER_LIMIT; ++i )
     {
-        events[i].data.fd = -1;
-        events[i].events = 0;
+        fds[i].fd = -1;
+        fds[i].events = 0;
     }
-    events[0].data.fd = listenfd;
-    events[0].events = EPOLLIN | EPOLLERR;
-	int epollfd = epoll_create();
+    fds[0].fd = listenfd;
+    fds[0].events = POLLIN | POLLERR;
+    fds[0].revents = 0;
+
     while( 1 )
     {
-        ret = epoll_wait( epollfd,events, user_counter+1, -1 );
+        ret = poll( fds, user_counter+1, -1 );
         if ( ret < 0 )
         {
-            printf( "epoll failure\n" );
+            printf( "poll failure\n" );
             break;
         }
     
-        for( int i = 0; i < ret; ++i )
+        for( int i = 0; i < user_counter+1; ++i )
         {
-            if( events[i].data.fd == listenfd ) )
+            if( ( fds[i].fd == listenfd ) && ( fds[i].revents & POLLIN ) )
             {
                 struct sockaddr_in client_address;
                 socklen_t client_addrlength = sizeof( client_address );
@@ -112,33 +101,35 @@ int main( int argc, char* argv[] )
                 user_counter++;
                 users[connfd].address = client_address;
                 setnonblocking( connfd );
-				addfd(epollfd,connfd,false);
+                fds[user_counter].fd = connfd;
+                fds[user_counter].events = POLLIN | POLLRDHUP | POLLERR;
+                fds[user_counter].revents = 0;
                 printf( "comes a new user, now have %d users\n", user_counter );
             }
-            else if( events[i].events & POLLERR )
+            else if( fds[i].revents & POLLERR )
             {
-                printf( "get an error from %d\n", events[i].data.fd );
+                printf( "get an error from %d\n", fds[i].fd );
                 char errors[ 100 ];
                 memset( errors, '\0', 100 );
                 socklen_t length = sizeof( errors );
-                if( getsockopt( events[i].data.fd, SOL_SOCKET, SO_ERROR, &errors, &length ) < 0 )
+                if( getsockopt( fds[i].fd, SOL_SOCKET, SO_ERROR, &errors, &length ) < 0 )
                 {
                     printf( "get socket option failed\n" );
                 }
                 continue;
             }
-            else if( events[i].events & POLLRDHUP )
+            else if( fds[i].revents & POLLRDHUP )
             {
-                users[events[i].data.fd] = users[events[user_counter].fd];
-                close( events[i].data.fd );
-                events[i] = events[user_counter];
+                users[fds[i].fd] = users[fds[user_counter].fd];
+                close( fds[i].fd );
+                fds[i] = fds[user_counter];
                 i--;
                 user_counter--;
                 printf( "a client left\n" );
             }
-            else if( events[i].events & POLLIN )
+            else if( fds[i].revents & POLLIN )
             {
-                int connfd = events[i].data.fd;
+                int connfd = fds[i].fd;
                 memset( users[connfd].buf, '\0', BUFFER_SIZE );
                 ret = recv( connfd, users[connfd].buf, BUFFER_SIZE-1, 0 );
                 printf( "get %d bytes of client data %s from %d\n", ret, users[connfd].buf, connfd );
@@ -147,8 +138,8 @@ int main( int argc, char* argv[] )
                     if( errno != EAGAIN )
                     {
                         close( connfd );
-                        users[events[i].data.fd] = users[events[user_counter].fd];
-                        events[i] = events[user_counter];
+                        users[fds[i].fd] = users[fds[user_counter].fd];
+                        fds[i] = fds[user_counter];
                         i--;
                         user_counter--;
                     }
@@ -159,33 +150,30 @@ int main( int argc, char* argv[] )
                 }
                 else
                 {
-					struct epoll_event event;
                     for( int j = 1; j <= user_counter; ++j )
                     {
-                        if( events[j].data.fd == connfd )
+                        if( fds[j].fd == connfd )
                         {
                             continue;
                         }
-						event.data.fd = events[j].data.fd;
-						event.events = events[j].events&(~EPOLLIN)|EPOLLOUT;
-						epoll_ctl( epollfd, EPOLL_CTL_MOD,event.data.fd, &event );
-                        users[events[j].fd].write_buf = users[connfd].buf;
+                        
+                        fds[j].events |= ~POLLIN;
+                        fds[j].events |= POLLOUT;
+                        users[fds[j].fd].write_buf = users[connfd].buf;
                     }
                 }
             }
-            else if( events[i].events & POLLOUT )
+            else if( fds[i].revents & POLLOUT )
             {
-                int connfd = events[i].fd;
+                int connfd = fds[i].fd;
                 if( ! users[connfd].write_buf )
                 {
                     continue;
                 }
                 ret = send( connfd, users[connfd].write_buf, strlen( users[connfd].write_buf ), 0 );
                 users[connfd].write_buf = NULL;
-				struct epoll_event event;
-				event.data.fd = events[i].data.fd;
-				event.events = events[j].events&(~EPOLLOUT)|EPOLLIN;
-				epoll_ctl( epollfd, EPOLL_CTL_MOD,event.data.fd, &event );
+                fds[i].events |= ~POLLOUT;
+                fds[i].events |= POLLIN;
             }
         }
     }
